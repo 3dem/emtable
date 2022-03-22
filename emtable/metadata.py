37182c9902ee7c9099d80dc9ff7cb13d2ece7741
Grigory Sharov <sharov.grigory@gmail.com>
2022-03-22 16:41:10 +0000

    bump version

--- emtable/metadata.py
+++ emtable/metadata.py
@@ -19,7 +19,7 @@
 # *
 # **************************************************************************
 
-__version__ = '0.0.9'
+__version__ = '0.0.10'
 __author__ = 'Jose Miguel de la Rosa Trevin, Grigory Sharov'
 
 
